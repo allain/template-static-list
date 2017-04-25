5028f41816608e274a58ea5f1518d44b2f34d8a6
allain <allain.lalonde@favequest.com>
2017-04-25 11:40:43 -0400

    using arguments instead of creating an unnecessary array

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 /*jshint esversion: 6, asi: true */
 
 module.exports = function (strings) {
-  var values = Array.prototype.slice.call(arguments)
-
   return strings.reduce((result, str, i) => {
     if (i === 0) {
-      result.push(str)
-    } else if (values[i] !== null && values[i] !== undefined) {
-      result.push(values[i])
+      result.push(str) // this is normally an empty string
+    } else if (arguments[i] !== null && arguments[i] !== undefined) {
+      result.push(arguments[i])
       result.push(str)
     } else if (i === strings.length - 1) {
       // If the last value is missing, then the previous delimeter must be popped
